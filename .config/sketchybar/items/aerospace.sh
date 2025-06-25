f575425e01bf6202a6bccf103e9ffd16aa019f61
Jakkapat Paijit <jakkapat.pai@ascendcorp.com>
2025-06-25 12:07:01 +0700

    🚩 Add aerospace worksapce 'N'

--- .config/sketchybar/items/aerospace.sh
+++ .config/sketchybar/items/aerospace.sh
@@ -2,7 +2,7 @@
 
 sketchybar --add event aerospace_workspace_change
 
-for sid in $(aerospace list-workspaces --all | grep -E '^(B|D|T|R)$'); do
+for sid in $(aerospace list-workspaces --all); do
     sketchybar --add item space.$sid left \
         --subscribe space.$sid aerospace_workspace_change \
         --set space.$sid \
